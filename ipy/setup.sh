b061665579871e7824dabf6a201388593e9d7e90
정갑기 <yag12@lycos.co.kr>
2016-12-12 13:27:24 +0900

    Update setup.sh

--- ipy/setup.sh
+++ ipy/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#############################################################################################################
+# ubuntu install
+#############################################################################################################
 sudo apt-get install python-pip python-dev build-essential 
 sudo pip install --upgrade pip 
 sudo pip install --upgrade virtualenv 
@@ -7,7 +10,7 @@ sudo pip install --upgrade virtualenv
 sudo apt-get install ipython
 sudo apt-get install ipython-notebook
 sudo apt-get install ipython-qtconsole
-echo "run : #] ipython notebook"
+echo "run : #] ipython notebook --ip 192.168.0.X"
 
 sudo apt-get install python-pandas
 sudo apt-get install python-numpy
@@ -18,3 +21,21 @@ cd apachelog-1.0/ && sudo python setup.py install
 
 sudo pip install runipy
 echo "run : #] runipy notebook.ipynb"
+
+#############################################################################################################
+# centOs install
+#############################################################################################################
+# rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+# yum install ipython
+# curl -k -O https://bootstrap.pypa.io/get-pip.py
+# python get-pip.py
+# 
+# pip install notebook
+# pip install qtconsole
+# pip install pandas
+# pip install scipy
+# pip install matplotlib
+# pip install cython
+# pip install runipy
+#
+# cd apachelog-1.0/ && sudo python setup.py install
